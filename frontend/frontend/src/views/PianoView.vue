2f34c42468d4d4c8e20aa2f305bf5fe53dfa0e4d
AarhamH <aarham.haider@gmail.com>
2023-08-28 16:44:30 -0700

    Separated piano keyboard view and component

---
+++ frontend/frontend/src/views/PianoView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="piano-container">
+    <pianoKeyboard> </pianoKeyboard>
+  </div>
+</template>
+
+<script>
+import PianoKeyboard from "../components/PianoKeyboardComponent.vue";
+import { mapActions } from "vuex";
+
+export default {
+  components: { PianoKeyboard },
+
+  created() {
+    this.createSampler();
+    this.createRecorder();
+  },
+
+  methods: {
+    ...mapActions("toneState", ["createSampler"]),
+    ...mapActions("recordingState", ["createRecorder"]),
+  },
+};
+</script>
+
+<style>
+#piano-container {
+  width: 100%;
+  background-color: black;
+}
+</style>
