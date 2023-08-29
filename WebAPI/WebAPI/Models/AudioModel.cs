b036a7efe4651cc3c4a5567e11c7f32dcf7a2097
AarhamH <aarham.haider@gmail.com>
2023-08-28 19:43:02 -0700

    Update AudioModel.cs

--- WebAPI/WebAPI/Models/AudioModel.cs
+++ WebAPI/WebAPI/Models/AudioModel.cs
@@ -1,6 +1,16 @@
-﻿namespace WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
 {
     public class AudioModel
     {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public DateTime Created { get; set; }
+
+        
+
     }
 }
