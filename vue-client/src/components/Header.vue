b23c630d6bb67e41206b594e2366e689694f8f93
AarhamH <aarham.haider@gmail.com>
2023-08-30 01:07:49 -0700

    Basic header

---
+++ vue-client/src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <nav class="container">
+            <div class="title">
+                <h1>Piano</h1>
+            </div>
+            <ul class="nav-routes">
+                <RouterLink to="/">  
+                    Home
+                </RouterLink>
+                <RouterLink to="about">  
+                    About
+                </RouterLink>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
