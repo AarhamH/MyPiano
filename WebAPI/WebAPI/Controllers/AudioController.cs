a8e35961f354eb37256a2ca8b2ca4fce49a8ffab
AarhamH <aarham.haider@gmail.com>
2023-08-29 22:31:43 -0700

    Implemented id get

--- WebAPI/WebAPI/Controllers/AudioController.cs
+++ WebAPI/WebAPI/Controllers/AudioController.cs
@@ -16,6 +16,15 @@ namespace WebAPI.Controllers
         [HttpGet]
         public async Task <IEnumerable<AudioModel>> Get()
             => await _context.Audios.ToListAsync();
+
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(AudioModel), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetByID(int id)
+        {
+            var audio = await _context.Audios.FindAsync(id);
+            return audio == null ? NotFound() : Ok(audio);
+        }
         
     }
 }
