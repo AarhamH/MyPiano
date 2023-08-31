75df849e72e859543fa078560dc7927e61194c03
AarhamH <aarham.haider@gmail.com>
2023-08-30 20:58:21 -0700

    Update HomeView.vue

--- vue-client/src/views/HomeView.vue
+++ vue-client/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="piano">
-    <div class="white-key" @click="playC4">A</div>
-    <div class="black-key" @click="playDb4">W</div>
-    <div class="white-key" @click="playD4">S</div>
-    <div class="black-key" @click="playEb4">E</div>
-    <div class="white-key" @click="playE4">D</div>
-    <div class="white-key" @click="playF4">F</div>
-    <div class="black-key" @click="playGb4">T</div>
-    <div class="white-key" @click="playG4">G</div>
-    <div class="black-key" @click="playAb4">Y</div>
-    <div class="white-key" @click="playA4">H</div>
-    <div class="black-key" @click="playBb4">U</div>
-    <div class="white-key" @click="playB4">J</div>
-    <div class="white-key" @click="playC5">K</div>
+    <div class="white-key" @click="playNote('C4')">A</div>
+    <div class="black-key" @click="playNote('Db4')">W</div>
+    <div class="white-key" @click="playNote('D4')">S</div>
+    <div class="black-key" @click="playNote('Eb4')">E</div>
+    <div class="white-key" @click="playNote('E4')">D</div>
+    <div class="white-key" @click="playNote('F4')">F</div>
+    <div class="black-key" @click="playNote('Gb4')">T</div>
+    <div class="white-key" @click="playNote('G4')">G</div>
+    <div class="black-key" @click="playNote('Ab4')">Y</div>
+    <div class="white-key" @click="playNote('A4')">H</div>
+    <div class="black-key" @click="playNote('Bb4')">U</div>
+    <div class="white-key" @click="playNote('B4')">J</div>
   </div>
 </template>
 
@@ -24,35 +23,20 @@ const playNote = (note) => {
   synth.triggerAttackRelease(note, "8n");
 };
 
-const playC4 = () => playNote("C4");
-const playDb4 = () => playNote("Db4");
-const playD4 = () => playNote("D4");
-const playEb4 = () => playNote("Eb4");
-const playE4 = () => playNote("E4");
-const playF4 = () => playNote("F4");
-const playGb4 = () => playNote("Gb4");
-const playG4 = () => playNote("G4");
-const playAb4 = () => playNote("Ab4");
-const playA4 = () => playNote("A4");
-const playBb4 = () => playNote("Bb4");
-const playB4 = () => playNote("B4");
-const playC5 = () => playNote("C5");
-
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
-    case "a": playC4(); break;
-    case "w": playDb4(); break;
-    case "s": playD4(); break;
-    case "e": playEb4(); break;
-    case "d": playE4(); break;
-    case "f": playF4(); break;
-    case "t": playGb4(); break;
-    case "g": playG4(); break;
-    case "y": playAb4(); break;
-    case "h": playA4(); break;
-    case "u": playBb4(); break;
-    case "j": playB4(); break;
-    case "k": playC5(); break;
+    case "q": playNote('C4'); break;
+    case "2": playNote('Db4'); break;
+    case "w": playNote('D4'); break;
+    case "3": playNote('Eb4'); break;
+    case "e": playNote('E4'); break;
+    case "4": playNote('F4'); break;
+    case "r": playNote('Gb4'); break;
+    case "5": playNote('G4'); break;
+    case "t": playNote('Ab4'); break;
+    case "6": playNote('A4'); break;
+    case "y": playNote('Bb4'); break;
+    case "7": playNote('B4'); break;
   }
 });
 </script>
