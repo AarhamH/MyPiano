d9012b8b0bd90c0abe49f413c605c595ea9d56ec
AarhamH <aarham.haider@gmail.com>
2023-09-10 17:46:33 -0700

    Implemented user repo

---
+++ WebAPI/WebAPI/Interfaces/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Interfaces
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context) 
+        {
+            _context = context;
+        }
+
+        public UserModel Create(UserModel user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user;
+        }
+    }
+}
