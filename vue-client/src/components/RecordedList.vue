5b91b0e0ae31f8ed6a4dbe0ac528348e04eec7b2
AarhamH <aarham.haider@gmail.com>
2023-10-05 22:01:58 -0700

    Re-styled recorded song table

--- vue-client/src/components/RecordedList.vue
+++ vue-client/src/components/RecordedList.vue
@@ -1,36 +1,42 @@
 <template>
-  <main class="st_viewport">
-    <div class="st_wrap_table" data-table_id="0">
-      <header class="st_table_header">
-        <h2>Table header one</h2>
-        <div class="st_row">
-          <div class="st_column _rank">ID</div>
-          <div class="st_column _name">Title</div>
-          <div class="st_column _surname">Date of Creation</div>
-          <div class="st_column _surname">Path</div>
-          <div class="st_column _surname">Action</div>
-        </div>
-      </header>
-      <div class="st_table">
-        <div class="st_row" v-for="(song, index) in songs" :key="index">
-          <div class="st_column _rank">{{ song.id }}</div>
-          <div class="st_column _name">{{ song.title }}</div>
-          <div class="st_column _surname">{{ song.created }}</div>
-          <div class="st_column _surname">{{ song.filePath }}</div>
-          <div>
+  <h1>Recorded Songs</h1>
+  <div class="tbl-header">
+    <table cellpadding="0" cellspacing="0">
+      <thead>
+        <tr>
+          <th>Index</th>
+          <th>Title</th>
+          <th>Date Of Creation</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+    </table>
+  </div>
+  <div class="tbl-content">
+    <table cellpadding="0" cellspacing="0">
+      <tbody v-for="(song, index) in songs" :key="index">
+        <tr>
+          <td>{{ index + 1 }} </td>
+          <td>{{ song.title }} </td>
+          <td>{{ song.created }}</td>
+          <td class="st_column_action">
             <audio
               ref="audioPlayer"
               id="audioPlayer"
-              controls:display="'none'"
-              autoplay
+              controls
+              :src="'https://localhost:7089/api/Audio/audio/' + song.title + '.webm'"             
             ></audio>
-            <button @click="playAudio(song.title)">Play</button>
-            <button @click="deleteSong(song.id, index)">Delete</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
+          </td>
+          <td>             
+            <button class="delete_button" @click="deleteSong(song.id, index)">
+              <img src="../assets/garbage.png" />
+            </button>
+          </td>        
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -95,125 +101,70 @@ export default {
         this.songs.splice(index, 1);
       }
     },
-
-    playAudio(audioFileName) {
-      const apiUrl = `https://localhost:7089/api/Audio/audio/${audioFileName}.webm`;
-
-      const audioPlayer = document.getElementById("audioPlayer");
-
-      audioPlayer.src = apiUrl;
-
-      audioPlayer.load();
-      audioPlayer.play();
-    },
   },
 };
 </script>
 
 <style lang="scss">
-h1 {
-  font-weight: 400;
+/////////////////////
+h1{
+  font-size: 25px;
+  color: #fff;
+  font-weight: 300;
+  text-align: center;
+  margin-bottom: 15px;
 }
-a {
-  color: inherit;
-}
-p {
-  margin-top: 0.7em;
-}
-.warning {
-  color: rgb(62, 148, 236);
-}
-.st_viewport {
-  max-height: 500px;
-  overflow: auto;
+.delete_button  {
+  background: transparent;
+  border: transparent;
+  cursor: pointer;
 }
 
-[data-table_id="1"] {
-  background-color: rgb(255, 115, 0);
+.delete_button img {
+  width:25%;
+  margin-top: 5px;
 }
-[data-table_id="2"] {
-  background-color: rgb(61, 53, 39);
-  color: rgb(220, 220, 220);
+table{
+  width:100%;
+  table-layout: fixed;
 }
-[data-table_id="3"] {
-  background-color: rgba(168, 189, 4, 0.8);
+.tbl-header{
+  background-color: rgba(255,255,255,0.3);
+ }
+.tbl-content{
+  height:250px;
+  overflow-x:auto;
+  margin-top: 0px;
+  border: 1px solid rgba(255,255,255,0.3);
 }
-
-._rank {
-  min-width: 80px;
+th{
+  padding: 20px 15px;
+  text-align: left;
+  font-weight: 500;
+  font-size: 12px;
+  color: #fff;
+  text-transform: uppercase;
 }
-._id {
-  min-width: 60px;
-}
-._name {
-  min-width: 130px;
-}
-._surname {
-  min-width: 130px;
-}
-._year {
-  min-width: 80px;
-}
-._section {
-  min-width: 130px;
-}
-
-pre {
-  overflow: auto;
-}
-
-/** Sticky table styles **/
-.st_viewport {
-  background-color: rgb(62, 148, 236);
-  color: rgb(27, 30, 36);
-  margin: 20px 0;
-}
-/* ##   header */
-.st_table_header {
-  position: -webkit-sticky;
-  position: sticky;
-  top: 0px;
-  z-index: 1;
-  background-color: rgb(27, 30, 36);
-  color: rgb(220, 220, 220);
-}
-.st_table_header h2 {
-  font-weight: 400;
-  margin: 0 20px;
-  padding: 20px 0 0;
-}
-.st_table_header .st_row {
-  color: rgba(220, 220, 220, 0.7);
-}
-/* ##  table */
-.st_table {
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  -webkit-flex-direction: column;
-  -ms-flex-direction: column;
-  flex-direction: column;
-}
-/* #   row */
-.st_row {
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -ms-flexbox;
-  display: flex;
-  margin: 0;
-}
-.st_table .st_row:nth-child(even) {
-  background-color: rgba(0, 0, 0, 0.1);
-}
-/* #   column */
-.st_column {
-  padding: 10px 20px;
+td{
+  padding: 10px;
+  text-align: left;
+  vertical-align:middle;
+  font-weight: 300;
+  font-size: 12px;
+  color: #fff;
+  border-bottom: solid 1px rgba(255,255,255,0.1);
 }
 
-audio {
-  width: 22%;
+
+/* for custom scrollbar for webkit browser*/
+
+::-webkit-scrollbar {
+    width: 6px;
+} 
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+} 
+::-webkit-scrollbar-thumb {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
 }
 </style>
